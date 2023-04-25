f68d231c0e8a505d76936b332dbfb3a31fa30250
StacyKangethe <stacykangethe18@gmail.com>
2023-04-25 11:05:37 +0300

    Add files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 
 const exchangeRates = {
@@ -44,4 +44,4 @@ module.exports = { convertCurrency };
 
 
 app.listen(port);
-console.log(`server is listening on port ${port}`);
+console.log("server listening on port 3000");
